7f8b00df265c566ac8586eb96b15ebad5d6ec4f1
divs-ray <32254452+divs-ray@users.noreply.github.com>
2017-12-01 12:43:09 -0600

    Add files via upload

--- Scraping_regression.py
+++ Scraping_regression.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[216]:
-
-
 # IPPP Final Project - Regression Analysis
 ### IMPACT of CCT Program on agricultural productivity variables ###
 # outcome vars are: ani1(owns farm animals), ani2(owns other animals), land(uses land)
@@ -17,25 +11,26 @@ import matplotlib.pyplot as plt
 inv_1 = pd.DataFrame(pd.read_stata("investments_data.dta", convert_categoricals=False))
 muni_data=pd.DataFrame(pd.read_csv("muni_manu_data.csv"))
 
-#Merge inv_1 with muni data on st_muni and convert 1998 data into integer for regression
+##Merge inv_1 with muni data on st_muni and convert 1998 data into integer for regression
+#Create the st_muni variable in Investment data
 inv_1['st_muni']=(inv_1['state'].astype(str).str.cat(inv_1['muni'].astype(str).str.zfill(3)))
 inv_1['st_muni']=inv_1['st_muni'].astype(int)
 
-
 muni_data=muni_data.rename(columns = {'code':'st_muni'})
 
-#replace , and trailing characters
+#replace ',' and trailing characters
 muni_data['1998']=muni_data['1998'].str.replace(',', '')
 muni_data['1998']=muni_data['1998'].str.strip('.00')
 
 muni_data['2003']=muni_data['2003'].str.replace(',', '')
 muni_data['2003']=muni_data['2003'].str.strip('.00')
 
+# merging on
 inv_2=pd.merge(inv_1, muni_data, on=('st_muni'), how='inner')
 
-#drop nas and convert to int
+#drop nas and convert to int and convert to billion pesos (original data was in '000 pesos)
 inv_2 = inv_2.dropna(subset = ['1998'])
-inv_2['1998']=inv_2['1998'].astype(int)
+inv_2['1998']=inv_2['1998'].astype(int)/1000000
 
 #mask waves 2,3, 4
 inv_2 = inv_2.loc[inv_2['wave'].isin([2,3,4])]
@@ -52,19 +47,12 @@ inv_2 = pd.concat([inv_2, inv_2_waves], axis=1)
 outcome_vars= ['ani1', 'ani2', 'land', 'vani1', 'vani2', 'ha']
 for i in outcome_vars:
 
-    x = inv_2[['t2_c1_op', 'nbani197', 'nbani297', 'ha97', '1998', 'age_hh', 'age_hh2', 'female_hh', 'educ1_hh', 
+    x = inv_2[['t2_c1_op', 'nbani197', 'nbani297', 'ha97', '1998', 'age_hh', 'age_hh2', 'female_hh', 'educ1_hh',
                'ethnicity_hh','age_sp', 'age_sp2', 'educ1_sp', 'dage0_7_97', 'dage8_17_97', 'dage18_54_97', 'dage55p_97',
-               'hhsize97', 'homeown97', 'dirtfloor97', 'electricity97', 'org_faenas', 'min_dist', 'lnup_cwagepm', 
-               'up2_mcwagepm', 'dummy_age_hh', 'dummy_educ_hh', 'dummy_ethnicity_hh', 'dummy_age_sp', 'dummy_educ_sp', 
+               'hhsize97', 'homeown97', 'dirtfloor97', 'electricity97', 'org_faenas', 'min_dist', 'lnup_cwagepm',
+               'up2_mcwagepm', 'dummy_age_hh', 'dummy_educ_hh', 'dummy_ethnicity_hh', 'dummy_age_sp', 'dummy_educ_sp',
                'dummy_dage0_7_97', 'dummy_dirtfloor97', 'dummy_electricity97']]
     y= inv_2[[i]]
     x = sm.add_constant(x)
     est = sm.OLS(y, x,missing='drop').fit()
     print(est.summary())
-
-
-# In[200]:
-
-
-inv_2['1998'].isnull().sum()
-
